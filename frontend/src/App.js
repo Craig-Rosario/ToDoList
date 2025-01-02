3634b81d3abbe01c80cd48025f85d93bca58c848
Craig Rosario <craig.rosario04@gmail.com>
2025-01-02 21:48:40 +0530

    Made footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar.jsx'
 import Home from './components/home/Home.jsx'
+import Footer from './components/footer/Footer.jsx'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Home/>
+      <Footer/>
     </div>
   )
 }
---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer container-fluid p-3 d-flex justify-content-center align-items-center text-white'>
+      <h4>tOdo</h4>&nbsp;<p className="m-0">&copy; tOdo.com</p>
+    </div>
+  )
+}
+
+export default Footer
