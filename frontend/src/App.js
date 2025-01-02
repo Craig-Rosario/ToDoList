0e5dc4d9462ba017b5e7801121f98989f3ffbb4a
Craig Rosario <craig.rosario04@gmail.com>
2025-01-03 00:05:10 +0530

    Added about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar.jsx'
 import Home from './components/home/Home.jsx'
 import Footer from './components/footer/Footer.jsx'
+import About from './components/about/About.jsx'
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <Home/>
+      <About/>
       <Footer/>
     </div>
   )
---
+++ frontend/src/components/about/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './About.css'
+const About = () => {
+  return (
+    <div className='about d-flex justify-content-center align-items-center'>
+      <div className='container'>
+        <div className='d-flex'>
+          <h1>About Us</h1>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque laborum temporibus voluptatum, perspiciatis consequuntur tempore consectetur? Atque, soluta? Adipisci non modi tempore distinctio in sapiente corporis unde expedita, numquam quas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi optio hic quas veritatis recusandae quaerat eaque ut harum, rerum provident ipsam, omnis voluptate illo pariatur, beatae officiis ullam libero ex.
+        <br />Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga saepe incidunt ipsum necessitatibus ex reiciendis ad, ab totam eos asperiores optio voluptas nulla consectetur officia suscipit delectus reprehenderit harum alias nobis molestiae molestias sed culpa. Esse minima facere repudiandae cum asperiores saepe error quasi, a nisi, ipsam, ullam eos facilis! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni asperiores animi beatae rem vel. Consequuntur nemo quidem quis ipsa ut.
+        </p>
+        
+
+      </div>
+    </div>
+  )
+}
+
+export default About
